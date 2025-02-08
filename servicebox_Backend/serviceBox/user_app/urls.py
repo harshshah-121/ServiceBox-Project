a9d9cd95e88d630264b674f7dc93d12e924769f4
YashSolanki92 <yashsolanki9265@gmail.com>
2025-02-08 11:05:54 +0530

    contact us api added

--- servicebox_Backend/serviceBox/user_app/urls.py
+++ servicebox_Backend/serviceBox/user_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegistrationView,DeleteAccountView,ChangePasswordView,UserLogoutView,UserProfileView,CSRFTokenView,ResetPasswordView,GetAllUsersView,UserLoginView,SendOTPView,VerifyOTPView
+from .views import UserRegistrationView,ContactUsView,DeleteAccountView,ChangePasswordView,UserLogoutView,UserProfileView,CSRFTokenView,ResetPasswordView,GetAllUsersView,UserLoginView,SendOTPView,VerifyOTPView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
@@ -13,4 +13,5 @@ urlpatterns = [
     path('user-logout/', UserLogoutView.as_view(), name='logout'),
     path('change-password/', ChangePasswordView.as_view(), name="chnage-password"),
     path('delete-account/', DeleteAccountView.as_view(), name='delete-account'),
+    path('contact-us/', ContactUsView.as_view(), name='contact_us'),
 ]
