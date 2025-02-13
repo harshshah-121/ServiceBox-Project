b66ab50e82bfbf9210716316b741887cf3f669fc
dhruv prajapati <dhruvprajapati792@gmail.com>
2025-02-13 22:35:46 +0530

    Add forward slash ("/") in urls.py

--- servicebox_Backend/serviceBox/Ser_provider/urls.py
+++ servicebox_Backend/serviceBox/Ser_provider/urls.py
@@ -3,5 +3,5 @@ from .views import ServiceProviderRegisterView,ServiceProvider_Main_Registration
 
 urlpatterns = [
     path('register/', ServiceProviderRegisterView.as_view(), name='service-provider-register'),
-    path('main_register', ServiceProvider_Main_Registration.as_view(), name='service-provider-main-register'),
+    path('main_register/', ServiceProvider_Main_Registration.as_view(), name='service-provider-main-register'),
 ]
