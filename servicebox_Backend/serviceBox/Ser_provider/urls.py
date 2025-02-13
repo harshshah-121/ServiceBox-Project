455e007154f2f2ff948b602ad822e359ef4a620b
dhruv prajapati <dhruvprajapati792@gmail.com>
2025-02-13 22:23:22 +0530

    add path in urls.py

--- servicebox_Backend/serviceBox/Ser_provider/urls.py
+++ servicebox_Backend/serviceBox/Ser_provider/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ServiceProviderRegisterView
+from .views import ServiceProviderRegisterView,ServiceProvider_Main_Registration
 
 urlpatterns = [
     path('register/', ServiceProviderRegisterView.as_view(), name='service-provider-register'),
+    path('main_register', ServiceProvider_Main_Registration.as_view(), name='service-provider-main-register'),
 ]
