cda8e67c707cc9322bf74217cc659dd0e3edfa84
dhruv prajapati <dhruvprajapati792@gmail.com>
2025-02-16 17:36:53 +0530

    delete file extention validator

--- servicebox_Backend/serviceBox/Ser_provider/models.py
+++ servicebox_Backend/serviceBox/Ser_provider/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.core.validators import FileExtensionValidator
+
 
 class ServiceProviderManager(BaseUserManager):
     def create_user(self, email, firstname, lastname, password=None):
